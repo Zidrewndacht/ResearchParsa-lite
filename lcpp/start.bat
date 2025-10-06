3c5cb0d10a23a015877022a2318803f489ad200f
Luis Alfredo <46092585+Zidrewndacht@users.noreply.github.com>
2025-10-06 01:44:46 -0300

    v0.8.16

--- lcpp/start.bat
+++ lcpp/start.bat
@@ -1,2 +1,3 @@
-C:\llama.cpp\binaries\llama-server --model "C:\llama.cpp\models\Qwen3-30B-A3B-Instruct-2507-UD-Q4_K_XL.gguf" --n-gpu-layers 99 --jinja --no-mmap --ctx_size 120000 -np 24 --alias Qwen30b-A3b-Q4 --cache-reuse 256
+rem Sampple llama.cpp launch command
+C:\llama.cpp\binaries\llama-server --model "C:\llama.cpp\models\Qwen3-30B-A3B-Instruct-2507-UD-Q4_K_XL.gguf" --n-gpu-layers 99 --jinja --no-mmap --ctx_size 131072 -np 4 --alias Qwen30b-A3b-Q4 --cache-reuse 256
 pause
